95818312f705061a1f4d50192c33781097f3d1ff
jamesngn <103204978+jamesngn@users.noreply.github.com>
2024-04-05 04:44:04 +1100

    revert 6ba3e4a17a51bab384c662811a0057fe0c5f798b

--- src/main/java/jamesngnm/travelbookingsystem/interfaces/UserManager.java
+++
@@ -1,7 +0,0 @@
-package jamesngnm.travelbookingsystem.interfaces;
-
-public interface UserManager {
-    void registerUser(String email, String password);
-    void loginUser(String email, String password);
-    void logoutUser();
-}
