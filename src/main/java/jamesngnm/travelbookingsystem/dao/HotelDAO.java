e802ec8eb18018a3d1137d961a352bf2536d0a6d
jamesngn <103204978+jamesngn@users.noreply.github.com>
2024-04-17 08:04:22 +1000

    feat: create hotel
    Signed-off-by: jamesngn <103204978+jamesngn@users.noreply.github.com>

--- src/main/java/jamesngnm/travelbookingsystem/dao/HotelDAO.java
+++ src/main/java/jamesngnm/travelbookingsystem/dao/HotelDAO.java
@@ -1,12 +1,12 @@
 package jamesngnm.travelbookingsystem.dao;
 
 import jamesngnm.travelbookingsystem.entity.HotelEntity;
-import jamesngnm.travelbookingsystem.model.request.GetHotelDetailsRequest;
 import jamesngnm.travelbookingsystem.model.request.SearchHotelRequest;
 
 import java.util.List;
 
 public interface HotelDAO {
+    void createHotel(HotelEntity hotel);
     List<HotelEntity> searchHotel(SearchHotelRequest searchHotelRequest);
     HotelEntity getHotelDetail(Long hotelId);
 }
