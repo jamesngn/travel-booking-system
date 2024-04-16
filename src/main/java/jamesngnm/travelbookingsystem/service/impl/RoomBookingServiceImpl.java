ae1416d66be89147318ae0e01fa4786b71700c7d
jamesngn <103204978+jamesngn@users.noreply.github.com>
2024-04-17 08:06:26 +1000

    refactor: add exception and error code
    Signed-off-by: jamesngn <103204978+jamesngn@users.noreply.github.com>

--- src/main/java/jamesngnm/travelbookingsystem/service/impl/RoomBookingServiceImpl.java
+++ src/main/java/jamesngnm/travelbookingsystem/service/impl/RoomBookingServiceImpl.java
@@ -7,6 +7,8 @@ import jamesngnm.travelbookingsystem.dao.impl.RoomDAOImpl;
 import jamesngnm.travelbookingsystem.entity.BookedDate;
 import jamesngnm.travelbookingsystem.entity.HotelBookingEntity;
 import jamesngnm.travelbookingsystem.entity.RoomEntity;
+import jamesngnm.travelbookingsystem.exception.BadRequestError;
+import jamesngnm.travelbookingsystem.exception.ResponseException;
 import jamesngnm.travelbookingsystem.service.RoomBookingService;
 import jamesngnm.travelbookingsystem.service.RoomService;
 
@@ -15,6 +17,11 @@ import java.util.Objects;
 public class RoomBookingServiceImpl implements RoomBookingService {
     private final RoomBookingDAO roomBookingDAO;
     private final RoomService roomService;
+
+    public RoomBookingServiceImpl(RoomBookingDAO roomBookingDAO, RoomService roomService) {
+        this.roomBookingDAO = roomBookingDAO;
+        this.roomService = roomService;
+    }
 
     public RoomBookingServiceImpl() {
         this.roomService = new RoomServiceImpl();
@@ -25,16 +32,16 @@ public class RoomBookingServiceImpl implements RoomBookingService {
     public void reserveRoom(Long roomId, HotelBookingEntity hotelBooking) {
         // Reserve room
         if (roomId == null) {
-            throw new IllegalArgumentException("Room ID not found");
+            throw new ResponseException(BadRequestError.ROOM_ID_INVALID);
         }
         if (hotelBooking == null) {
-            throw new IllegalArgumentException("Hotel booking not found");
+            throw new ResponseException(BadRequestError.HOTEL_BOOKING_NOT_FOUND);
         }
 
         RoomEntity room = roomService.getRoomById(roomId);
 
         if (!Objects.equals(room.getHotel().getId(), hotelBooking.getHotel().getId())) {
-            throw new IllegalArgumentException("Room does not belong to hotel");
+            throw new ResponseException(BadRequestError.ROOM_NOT_BELONG_TO_HOTEL);
         }
 
         roomBookingDAO.createRoomBooking(room, hotelBooking);
