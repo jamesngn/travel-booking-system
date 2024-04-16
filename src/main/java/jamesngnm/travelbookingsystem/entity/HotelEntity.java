0a44ed6738256f8722e49f33b05c03e3a5c4c835
jamesngn <103204978+jamesngn@users.noreply.github.com>
2024-04-17 08:04:04 +1000

    feat: change ID generation strategy
    Signed-off-by: jamesngn <103204978+jamesngn@users.noreply.github.com>

--- src/main/java/jamesngnm/travelbookingsystem/entity/HotelEntity.java
+++ src/main/java/jamesngnm/travelbookingsystem/entity/HotelEntity.java
@@ -14,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 public class HotelEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String location;
