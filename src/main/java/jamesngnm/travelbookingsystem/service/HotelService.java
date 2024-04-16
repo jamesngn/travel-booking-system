1c6e686e6067a2ae5f6383ec061f1395b42afe2f
jamesngn <103204978+jamesngn@users.noreply.github.com>
2024-04-17 08:04:31 +1000

    feat: create hotel
    Signed-off-by: jamesngn <103204978+jamesngn@users.noreply.github.com>

--- src/main/java/jamesngnm/travelbookingsystem/service/HotelService.java
+++ src/main/java/jamesngnm/travelbookingsystem/service/HotelService.java
@@ -1,6 +1,7 @@
 package jamesngnm.travelbookingsystem.service;
 
 import jamesngnm.travelbookingsystem.entity.HotelEntity;
+import jamesngnm.travelbookingsystem.model.request.CreateHotelRequest;
 import jamesngnm.travelbookingsystem.model.request.GetHotelDetailsRequest;
 import jamesngnm.travelbookingsystem.model.request.SearchHotelRequest;
 import jamesngnm.travelbookingsystem.model.response.HotelDetailResponse;
@@ -9,6 +10,7 @@ import jamesngnm.travelbookingsystem.model.response.SearchHotelResponse;
 import java.util.List;
 
 public interface HotelService {
+    void createHotel(CreateHotelRequest createHotelRequest);
     List<SearchHotelResponse> searchHotel(SearchHotelRequest searchHotelRequest);
     HotelEntity getHotelEntityById(Long hotelId);
     HotelDetailResponse getHotelDetail(GetHotelDetailsRequest getHotelDetailsRequest);
